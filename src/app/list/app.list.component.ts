e4aa15987d999d2c720d3271135be2dcf4346857
FabioNgo <ngolebaotrung@gmail.com>
2018-03-30 18:13:12 +0800

    Add container template structure for list page

--- src/app/list/app.list.component.ts
+++ src/app/list/app.list.component.ts
@@ -7,12 +7,46 @@ import 'rxjs/add/operator/switchMap';
   templateUrl: './app.list.component.html',
   styleUrls: ['./app.list.component.css']
 })
-export class AppListComponent {
+export class AppListComponent implements OnInit {
   router: Router;
   route: ActivatedRoute;
+  menus = [
+    {
+      name: 'Cases',
+      id: '1',
+      link: 'cases',
+      selected: true
+    },
+    {
+      name: 'Create',
+      id: '2',
+      link: '#',
+      selected: false
+    },
+    {
+      name: 'Admin',
+      id: '3',
+      link: '#',
+      selected: false
+    }
+  ];
 
-  constructor(router: Router) {
+  constructor(router: Router, route: ActivatedRoute) {
     this.router = router;
-    // this.route = route;
+    this.route = route;
+  }
+
+  ngOnInit(): void {
+    // const temp = this.route.params.value;
+  }
+
+  clickItem(menu) {
+    for (let i = 0; i < this.menus.length; i++) {
+      if (this.menus[i].id === menu.id) {
+        this.menus[i].selected = true;
+      } else {
+        this.menus[i].selected = false;
+      }
+    }
   }
 }
